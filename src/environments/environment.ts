98f0458bf16b20229ea698890c8aa725e7a59c5a
Jim McConnell <jimwmcconnell@yahoo.com>
2018-05-22 20:54:41 -0500

    added finished app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAmgM9Ysvwb5P04VTT5duZjJR6j_Ssiokc",
+    authDomain: "clientpanelprod-6f8c0.firebaseapp.com",
+    databaseURL: "https://clientpanelprod-6f8c0.firebaseio.com",
+    projectId: "clientpanelprod-6f8c0",
+    storageBucket: "clientpanelprod-6f8c0.appspot.com",
+    messagingSenderId: "159585641908"
+  }
 };
